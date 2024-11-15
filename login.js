6ebf01e91b69c9102a02fea764a79b09963e5b6c
[firstname lastname] <kk7012234@gmail.com>
2024-11-15 15:56:23 +0530

    loginpage

---
+++ login.js
@@ -0,0 +1,28 @@
+let login = document.querySelector(".login input");
+let password = document.querySelector(".pass input");
+let button = document.querySelector("button");
+let form = document.querySelector(".loginForm");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+});
+
+
+const user = "admin";
+const pass = "admin";
+
+alert("Use Below Details to Login\nUserName : admin\nPass : admin");
+ 
+button.addEventListener("click", ()=>{
+     check();
+})
+
+let check =  ()=>{
+    if(login.value == user && password.value == pass){
+        alert("login success");
+        window.location.href="home.html";
+    }else{
+        alert("Please Enter Valid UserName and Password");
+        window.location.href="login.html";
+    }
+}
