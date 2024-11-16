73970383a613c62b96327969dda2a757d8178f83
[firstname lastname] <kk7012234@gmail.com>
2024-11-16 12:26:38 +0530

    side-bar-fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // alert("Hello Welcome to My Page");
 
-let bar = document.querySelector('.nav-list i');
-let sidebar = document.querySelectorAll('.sidebar a');
+let bar = document.querySelector(".nav-list i");
+let sidebar = document.querySelectorAll(".sidebar a");
 
-bar.addEventListener('click', function() {
-  const content = document.querySelector('.sidebar');
-  if (content.style.display === 'none' || content.style.display === '') {
-    content.style.display = 'flex';
+bar.addEventListener("click", function () {
+  const content = document.querySelector(".sidebar");
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "flex";
   } else {
-    content.style.display = 'none';
+    content.style.display = "none";
   }
 });
 
-sidebar.forEach((all)=>{
-  all.addEventListener("click", (e)=>{
+sidebar.forEach((all) => {
+  all.addEventListener("click", (e) => {
     alert("Rukk jaa BSDK Code Kar raha hu😂😂\nWhy in rush ??");
-  })
-})
+  });
+});
